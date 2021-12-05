d1e3ac1362b93c44a141a6e0f2d5ad9cf89b9757
Jhow Magnum Nascimento <jhokwmagnum@gmail.com>
2021-12-05 16:02:28 -0300

    Scanner adicionado para classe ContaCorrente

--- src/Executor.java
+++ src/Executor.java
@@ -1,29 +1,45 @@
-
+import java.util.Scanner;
 public class Executor {
 
 	public static void main(String[] args) {
 		// SET configurar(colocar) dados
 		// GET obter(pegar) dados
-				
+		
+		Scanner	dados = new Scanner(System.in);	
 		
 		
 		// === Dados do Cliente ===
-		Cliente nomeCliente = new Cliente();
-		System.out.println("--- Dados do Cliente ---");
-		nomeCliente.MostrarCliente();
+		//Cliente nomeCliente = new Cliente();
+		//System.out.println("--- Dados do Cliente ---");
+		//nomeCliente.MostrarCliente();
 		
 		
 		
 		// === Dados da Conta Corrente ===
 		ContaCorrente CC0 = new ContaCorrente();
-		CC0.setNumero_doc_cc(23456789);
-		CC0.setAgencia_cc(100);
-		CC0.setConta_cc(100);
-		CC0.setValor_cc(50.0f);
-		CC0.setTipo_cc(1);
-		CC0.setData_ocorrencia_cc("20/10/2021");
-		CC0.setSaldo_cc(100.0f);
+		System.out.printf("Digite o número do documento: ");
+		CC0.setNumero_doc_cc(dados.nextInt()); 
 		
+		System.out.printf("Digite o número da agência: ");
+		CC0.setAgencia_cc(dados.nextInt());
+		
+		System.out.printf("Digite o número da conta: ");
+		CC0.setConta_cc(dados.nextInt());
+		
+		System.out.printf("Digite o valor: ");
+		CC0.setValor_cc(dados.nextFloat());
+		
+		System.out.print("Digite o tipo da conta: ");
+		CC0.setTipo_cc(dados.nextInt());
+		
+		System.out.print("Digite a data de Ocorrência: ");
+		CC0.setData_ocorrencia_cc(dados.next());
+		
+		System.out.print("Digite o valor do saldo: ");
+		CC0.setSaldo_cc(dados.nextFloat());
+		
+		
+		// Impressão
 		System.out.println("\n--- Conta Corrente ---");
 		System.out.println("Número Documento: " + CC0.getNumero_doc_cc());
 		System.out.println("Agência: " + CC0.getAgencia_cc());
